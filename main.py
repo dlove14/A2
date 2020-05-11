73ca96fb4f396ca92ada464aff017f59e3f4e264
dlove14 <danielle.love145@gmail.com>
2020-05-11 17:55:12 -0600

    test1 to see if password of <8 length is rejected correctly

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import unittest
+import string
 from task import check_pwd
 
 
 class TestCase(unittest.TestCase):
-    pass
+
+    def test1(self):
+        pwd = "525225"
+        self.assertFalse(check_pwd(pwd), "error")
 
 
 if __name__ == '__main__':
