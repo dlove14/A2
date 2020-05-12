627869eac7637aca42de0c7babdf648e28d5d396
dlove14 <danielle.love145@gmail.com>
2020-05-11 18:06:36 -0600

    wrote test3 to see if all lowercase password is rejected correctly

--- main.py
+++ main.py
@@ -13,6 +13,10 @@ class TestCase(unittest.TestCase):
         pwd = "ADBCHEDJID"
         self.assertFalse(check_pwd(pwd), "error")
 
+    def test3(self):
+        pwd = "fahfkbaffawfaw"
+        self.assertFalse(check_pwd(pwd), "error")
+        
 
 if __name__ == '__main__':
     unittest.main()
