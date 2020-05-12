bbc074b43467c3678f76d6bf17cdb6d8cd53ed8e
dlove14 <danielle.love145@gmail.com>
2020-05-11 18:13:46 -0600

    made check_pwd return false if no symbols are in the pwd

--- task.py
+++ task.py
@@ -1,4 +1,5 @@
 def check_pwd(pwd):
+    symbols = '~`!@#$%^&*()_+-='
     if len(pwd) <= 8 or len(pwd) >= 20:
         return False
     if not any(char.islower() for char in pwd):
@@ -7,4 +8,7 @@ def check_pwd(pwd):
         return False
     if not any(char.isdigit() for char in pwd):
         return False
+    if not any(char in symbols for char in pwd):
+        return False
+
     return True
